85e6749c8be24b967e23300beefefcd99ec4846b
Sahil <smppatel999@gmail.com>
2022-04-14 11:34:32 +0530

    Add tailwindcss file in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,11 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
 
+css: ['~/assets/css/tail.css'],
+  build: {
+    postcss: {
+      postcssOptions: require('./postcss.config.js'),
+    },
+  }
+
 })
