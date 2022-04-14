4c71dc13f8b0758f320bf5e5adfe6a27a922107b
Sahil <smppatel999@gmail.com>
2022-04-14 11:33:44 +0530

    Add tailwindcss support

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './assets/**/*.{vue,js,css}',
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+//    './nuxt.config.{js,ts}',
+  ],
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
