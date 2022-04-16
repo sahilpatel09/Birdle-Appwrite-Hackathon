4d2d96bb2b746a13eee0507e26443aba1496cf4f
Sahil <smppatel999@gmail.com>
2022-04-16 11:57:44 +0530

    Add siteBaseUrl in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,13 @@ import { defineNuxtConfig } from "nuxt3";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  
-  privateRuntimeConfig: {
-    API_BASE_URL: process.env.APPWRITE_URL
-},
-
+  runtimeConfig: {
+    baseSiteURL: 'https://medium.termshel.com',
+    public: {
+      apiBase: process.env.APPWRITE_URL,
+      siteBase: process.env.SITE_BASE,
+    }
+  },
   css: ["~/assets/css/tail.css"],
   
   build: {
