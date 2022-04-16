8d4695749ed636febe766461977bdd67d44e9d1d
Sahil <smppatel999@gmail.com>
2022-04-16 11:58:35 +0530

    Add composables support

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,39 @@
 <template>
-  <div>
+  <div v-if="loggedin">
     <NavHero />
     <TrendingPosts />
     <hr class="border border-gray-50 mb-10" />
-    <LoginModel :class="{ hidden: isMenuOpen }" class="mb-16" />
+    <LoginModel :class="{ hidden: hpToggler }" class="mb-16" />
     <PostwithSidebar />
   </div>
 </template>
 
 <script setup>
-const isMenuOpen = useState("open", () => {
-  return true;
-});
+const { appwrite } = useAppwrite()
+const router = useRouter();
+const loggedin = ref(false);
 
-// const config = useRuntimeConfig();
-// console.log(config.API_BASE_URL)
+const { hpToggler } = stateManager()
+
+
+onBeforeMount(()=>{
+  
+  appwrite
+          .account.get()
+          .then((user) => {
+            router.push('/me')
+          })
+          .catch((err) => {
+            loggedin.value = true
+            console.log(loggedin.value)   
+          });
+
+})
+
+  
+
+  
+
 </script>
 
 <style scoped></style>
