3792020e676e666bc40bb45ce020ede8c0af8599
Sahil <smppatel999@gmail.com>
2022-04-16 13:45:28 +0530

    Add loading logo to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,15 @@
 <template>
+  <div>
   <div v-if="loggedin">
     <NavHero />
     <TrendingPosts />
     <hr class="border border-gray-50 mb-10" />
     <LoginModel :class="{ hidden: hpToggler }" class="mb-16" />
     <PostwithSidebar />
+  </div>
+  <div v-else class="flex items-center justify-center fixed inset-0 left-0 right-0 top-0 bottom-0">
+    <img src="@/assets/img/medium-logo.png" class="w-20" alt="">
+  </div>
   </div>
 </template>
 
